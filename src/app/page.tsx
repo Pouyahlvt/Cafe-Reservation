bba3af96c88e2d6396c1fd43385cc5fba34c2a97
pouya <pouyahalavat@gmail.com>
2026-07-17 22:04:18 +0330

    i create menu section and pick colors and font !

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import Intro_section from "../components/home/intro";
+import Menu from "../components/menu";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen bg-olive-700 ">
       <div className="fixed top-0 w-full">{<Intro_section />}</div>
-      <div className="absolute top-full w-full z-20 h-screen bg-white rounded-t-4xl"></div>
+      <div className="absolute top-full w-full z-20 h-screen bg-transparent">
+        {<Menu />}
+      </div>
     </div>
   );
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,22 +1,30 @@
 "use client";
 
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+type Menu_tems_type = {
+  [category: string]: {
+    [item: string]: string[];
+  };
+};
 
 const Menu = () => {
-  const menu_hot_drink: string[] = [
-    "espresso ",
-    "capochino",
-    "turkish coffee",
-    "late",
-    "amreicano",
-    "tea",
-    "caramel makiato",
-    "macha",
-  ];
+  const menu_items: Menu_tems_type = {
+    "hot drinks": {
+      late: ["espresso", "milk"],
+      capochino: ["espresso", "milk", "fome milk"],
+    },
+    "cold dronks": { "ice late": ["espresso", "milk", "ice"] },
+    shakes: { "nutella shake": ["milk", "nutella", "banana", "cream"] },
+    breakfast: { egg: ["just egg idiot !"] },
+    food: { pasta: ["pasta", "parmesan", "chiken", "milk", "cream"] },
+  };
   return (
-    <section className="menu w-full h-fit">
+    <section className="menu w-full h-screen bg-dark-spruce rounded-t-4xl pt-25">
+      <h1 className="text-[10rem] tracking-tighter text-forest-moss text-center font-black font-museo">
+        Menu
+      </h1>
       <div></div>
     </section>
   );
 };
+
+export default Menu;
