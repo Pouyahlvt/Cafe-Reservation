5273b0dcf6be61c493f97f1d959a90b46f63ca74
pouya <pouyahalavat@gmail.com>
2026-08-11 22:53:27 +0330

    save progress again :(

--- src/components/ui/cafemap.tsx
+++ src/components/ui/cafemap.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import Image from "next/image";
+import { EightTable, TableSix, TableFour, Table } from "./cafemapui/tabels";
+
 const CafeMap = () => {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen ">
       <Image
         width={1400}
-        height={10}
+        height={1050}
         src={"/cafefloor.jpg"}
         alt={"cafe floor"}
-        className="h-full object-cover fix flex z-0"
+        className="max-md:h-full max-md:object-cover flex z-0"
       />
+      <Table table_num={8} x={340} y={35} size={220} reserved={false} />
+      <Table table_num={8} x={565} y={35} size={220} reserved={false} />
+      <Table table_num={8} x={795} y={35} size={220} reserved={false} />
+      <Table table_num={6} x={385} y={780} size={200} reserved={false} />
+      <Table table_num={6} x={585} y={780} size={200} reserved={false} />
+      <Table table_num={6} x={785} y={780} size={200} reserved={false} />
+      <Table table_num={4} x={450} y={300} size={200} reserved={false} />
+      <Table table_num={4} x={650} y={300} size={200} reserved={false} />
+      <Table table_num={4} x={450} y={500} size={200} reserved={false} />
+      <Table table_num={4} x={650} y={500} size={200} reserved={false} />
+      <Table table_num={12} x={850} y={330} size={200} reserved={false} />
+      <Table table_num={2} x={1050} y={260} size={200} reserved={false} />
+      <Table table_num={2} x={1050} y={360} size={200} reserved={false} />
+      <Table table_num={2} x={1050} y={460} size={200} reserved={false} />
+      <Table table_num={2} x={1050} y={560} size={200} reserved={false} />
     </div>
   );
 };
