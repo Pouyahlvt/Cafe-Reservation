1b1e38e8dd9ed6c7d2036514523ed521bf4b07a8
pouya <pouyahalavat@gmail.com>
2026-08-17 00:07:13 +0330

    table now come from api !

--- src/app/reservation/map/page.tsx
+++ src/app/reservation/map/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import CafeMap from "@/src/components/ui/cafemap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
@@ -15,103 +15,28 @@ interface TableData {
 }
 
 export default function ChooseTablePage() {
-  const tableData: TableData[] = [
-    // 8-person tables (top row)
-    { table_id: 8001, table_num: 8, x: 355, y: 55, size: 225, reserved: false },
-    { table_id: 8002, table_num: 8, x: 585, y: 55, size: 225, reserved: false },
-    { table_id: 8003, table_num: 8, x: 820, y: 55, size: 225, reserved: false },
-
-    // 6-person tables (bottom row)
-    {
-      table_id: 6001,
-      table_num: 6,
-      x: 405,
-      y: 810,
-      size: 200,
-      reserved: false,
-    },
-    { table_id: 6002, table_num: 6, x: 605, y: 810, size: 200, reserved: true },
-    {
-      table_id: 6003,
-      table_num: 6,
-      x: 805,
-      y: 810,
-      size: 200,
-      reserved: false,
-    },
-
-    // 4-person tables (middle-left)
-    {
-      table_id: 4001,
-      table_num: 4,
-      x: 500,
-      y: 330,
-      size: 120,
-      reserved: false,
-    },
-    { table_id: 4002, table_num: 4, x: 700, y: 330, size: 120, reserved: true },
-    {
-      table_id: 4003,
-      table_num: 4,
-      x: 500,
-      y: 550,
-      size: 120,
-      reserved: false,
-    },
-    {
-      table_id: 4004,
-      table_num: 4,
-      x: 700,
-      y: 550,
-      size: 120,
-      reserved: false,
-    },
-
-    // 12-person table
-    {
-      table_id: 1201,
-      table_num: 12,
-      x: 900,
-      y: 350,
-      size: 140,
-      reserved: false,
-    },
-
-    // 2-person tables (right side)
-    {
-      table_id: 2001,
-      table_num: 2,
-      x: 1130,
-      y: 330,
-      size: 120,
-      reserved: false,
-    },
-    {
-      table_id: 2002,
-      table_num: 2,
-      x: 1130,
-      y: 430,
-      size: 120,
-      reserved: true,
-    },
-    {
-      table_id: 2003,
-      table_num: 2,
-      x: 1130,
-      y: 530,
-      size: 120,
-      reserved: false,
-    },
-    {
-      table_id: 2004,
-      table_num: 2,
-      x: 1130,
-      y: 630,
-      size: 120,
-      reserved: false,
-    },
-  ];
+  const [table_data, setTable_data] = useState<TableData[]>([]);
   const [selected, setSelected] = useState("");
+
+  useEffect(() => {
+    const date = sessionStorage.getItem("date");
+    const meal = sessionStorage.getItem("meal");
+
+    try {
+      async function getTable() {
+        const res = await fetch(`/api/tables?date=${date}&meal=${meal}`);
+
+        if (!res.ok) return;
+
+        const data = await res.json();
+
+        setTable_data(data);
+      }
+      getTable();
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
 
   useGSAP(() => {
     gsap.to(".first-mass", {
@@ -159,7 +84,7 @@ export default function ChooseTablePage() {
     <div className="w-full h-screen bg-dark-spruce">
       <div
         className={`first-mass fixed w-60 h-20 top-5 left-4 flex z-50 border-muted-teal border-2 rounded-full 
-          bg-muted-teal/30 backdrop-blur-md items-center justify-center -translate-y-30 `}>
+          bg-muted-teal/30 backdrop-blur-md items-center justify-center -translate-y-30`}>
         <p className="text-2xl text-muted-teal font-museo font-bold tracking-tighter select-none">
           Choose a Table !
         </p>
@@ -172,8 +97,9 @@ export default function ChooseTablePage() {
         onClick={() => setSelected("")}>
         Delete Table
       </button>
+      <div></div>
       <CafeMap
-        tableData={tableData}
+        tableData={table_data}
         selected={selected}
         setSelected={setSelected}
       />
